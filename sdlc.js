c796af004f30e2ea152223bd8f11858c36c6a8d2
verhaq <haqverdah98@gmail.com>
2022-11-04 13:56:28 -0500

    completed sdlc file

--- sdlc.js
+++ sdlc.js
@@ -14,10 +14,22 @@
 
 console.log("planning, analysis of requirements, design, implementation, testing & integration, maintain")
 
+// CORRECTED STEPS:
+// 1. Planning
+// 2. Analysis
+// 3. Design
+// 4. Implementation
+// 5. testing and integration
+// 6. maintanence 
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const analysis of requirements = " "
+const analysis = "Step 2: gathering details required for a new system and determinig first ideas for prototypes. "
+const design = "Step 3: Outline details for overall application."
+const implementation = "Step 4: Writing the code and building the application according to specifications outlined in the design step."
+const testing = "Step 5: Testing the software and ensuring that it runs smoothly and performs tasks correctly without any bugs."
+const maintain = "Step 6: Making sure that the software continues to function properly after deployment. This includes correcting residual bugs or fixing other issues consumers may face while using the product."
