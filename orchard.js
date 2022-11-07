64ccc58c40a8d99817f8d765fd5b1da55fc8e2f9
verhaq <haqverdah98@gmail.com>
2022-11-06 20:19:16 -0600

    final completed orchard file

--- orchard.js
+++ orchard.js
@@ -76,7 +76,7 @@ var totalAcres = sumFuji + sumGala + sumPink
 console.log(totalAcres)
 
 // COMMENTS: I looked up how to get the sum of an array of numbers, then I calculated the sum of each array and at the end
-// I added all those arrays together to get the total number. I do not know how to add multiple arrays to each other.
+// I added all those arrays together to get the total number. 
 
 
 
@@ -144,12 +144,17 @@ let days = 0
 
 // CODE HERE
 
-// while (acresLeft > 0) {
-//     acresLeft - averageDailyAcres
-// days += 1
-// }
+ while (acresLeft > 0) {
+     acresLeft -= averageDailyAcres
+ days += 1
+ }
 
-//console.log(days)
+console.log(days)
+
+//COMMENT: This loop will start at 174 and stop once acresLeft equals 1 because of the while statement being 'acresLeft > 0'. For every subtraction of averageDailyAcres
+//from acresLeft, it will add 1 in increments to the days variable. According to the output, there will be 20 days left until there are 0 acresLeft. When you actually calculate it,
+//by the 19th day, there are 5 acresLeft. So on the 20th day, those last 5 acres are subtracted by the 9 averageDailyAcres, making acresLeft now 0 (technically a negative number), 
+//which stops the while loop and leavs the days variable at a value of 20. 
 
 
 // PROBLEM 4
@@ -178,9 +183,9 @@ let days = 0
 
 // CODE HERE
 
-// let fujiTons =
-// let galaTons =
-// let pinkTons =
+ let fujiTons = []
+ let galaTons = []
+ let pinkTons = []
 
 // const fujiAcres = [2, 3, 3, 2, 2, 2, 1]
 // const galaAcres = [5, 2, 4, 3, 6, 2, 4]
@@ -189,16 +194,37 @@ let days = 0
 // const fujiPrice = .89 
 // const galaPrice = .64
 // const pinkPrice = .55
-let tonFuji = 6.5
+
+
+let multiple = 6.5
 for (let i = 0; i < fujiAcres.length; i++) {
-    tonFuji *= fujiAcres[i]
-    break
+    fujiTons.push(multiple * fujiAcres[i])
 }
 
-console.log(tonFuji)
-// I know I'm supposed to multiply 6.5 by each number in the arrays to get individual values for a new array. But I don't know how to do that. 
-// I want the for loop to keep going after the break 
-// for each value, but I don't know how to do that either. I feel really dumb. 
+console.log(fujiTons)
+
+for (let i = 0; i < galaAcres.length; i++) {
+    galaTons.push(multiple * galaAcres[i])
+}
+
+console.log(galaTons)
+
+for (let i = 0; i < pinkAcres.length; i++) {
+    pinkTons.push(multiple * pinkAcres[i])
+}
+
+console.log(pinkTons)
+
+// COMMENTS: I know I'm supposed to multiply 6.5 by each value in the original "const ___Acres" arrays and push the new values into the "let __Tons" arrays. My original code was 
+// let multiple = 6.5
+// for (let i = 0; i < fujiAcres.length; i++) {
+//    fujiAcres[i] = multiple * fujiAcres[i]
+//    fujiTons.push(fujiAcres)
+//}
+// BUT this was printing the new arrays 7 times because thats how many values are in the original array. I'm not really sure WHY this was happening. 
+// To fix it, I decided to try to push the formula into the new array so that there would be only 1 array being printed. 
+
+
 
 // PROBLEM 5
 
@@ -218,10 +244,46 @@ console.log(tonFuji)
 
 // CODE HERE 
 
-// let fujiPounds =
-// let galaPounds =
-// let pinkPounds =
 
+// COMMENTS: First, I will use the same formula I used in problem 1 to get the sum of each array. 
+
+//let sumFuji = 0
+// for (let i = 0; i<fujiAcres.length; i++) {
+//     sumFuji += fujiAcres[i]
+
+
+
+let sumFujiTons = 0
+for (let i = 0; i < fujiTons.length; i++) {
+    sumFujiTons += fujiTons[i]
+}
+
+
+
+
+let sumGalaTons = 0
+for (let i = 0; i < galaTons.length; i++) {
+    sumGalaTons += galaTons[i]
+}
+
+
+let sumPinkTons = 0
+for (let i = 0; i < pinkTons.length; i++) {
+    sumPinkTons += pinkTons[i]
+}
+
+//COMMENTS: The sums in tons are as follows: sumFujiTons = 97.5, sumGalaTons = 169, sumpinkTons = 143. Now, to get the sum in pounds, I will multiply each of the tons by 2000 
+// because there are 2000 pounds in a ton. 
+
+
+let fujiPounds = (sumFujiTons * 2000)
+console.log(fujiPounds)
+
+let galaPounds = (sumGalaTons * 2000)
+console.log(galaPounds)
+
+let pinkPounds = (sumPinkTons * 2000)
+console.log(pinkPounds)
 
 
 
@@ -245,15 +307,23 @@ console.log(tonFuji)
 
 // CODE HERE
 
-// let fujiProfit =
-// let galaProfit =
-// let pinkProfit =
+let fujiProfit = (fujiPounds * fujiPrice)/100
+console.log(fujiProfit)
+
+let galaProfit = (galaPounds * galaPrice)/100
+console.log(galaProfit)
+
+let pinkProfit = (pinkPounds * pinkPrice)/100
+console.log(pinkProfit)
 
 // const fujiPrice = .89 
 // const galaPrice = .64
 // const pinkPrice = .55
 
-//COMMENTS: for this i would multiply each apple variety's price by the total pounds per variety. 
+//COMMENTS: To get the profit of each variety, I multiplied the price of each by the amount in pounds. That gave me the profit in cents, so I divided that by 100 to get the profit in dollars 
+// because there are 100 cents in a dollar. 
+
+
 
 
 
@@ -270,7 +340,7 @@ console.log(tonFuji)
 
 // CODE HERE//
 
-// COMMENTS: If i had values for these numbers these are the formulas i would use.
+// COMMENTS: To get the totalProfit, I just made a variable for it where within it, I added the individual profits I obtained in the last problem. 
 
-var totalProfit = fujiProfit + galaProfit + pinkProfit
-console.log(totalProfit)
+ var totalProfit = fujiProfit + galaProfit + pinkProfit
+ console.log(totalProfit)
